273279ff711a2615afdb9ca002c69667703f1998
Carl Limyao <carl.limyao@lsiowa.org>
2012-02-01 16:30:31 -0600

    nil? instead of nil

--- lib/javert/connection.rb
+++ lib/javert/connection.rb
@@ -14,7 +14,7 @@ module Javert
       #Javert.connection = Net::LDAP.new(config)
       conn = Net::LDAP.new
       conn.host = config["host"]
-      conn.port = config["port"] unless config["port"].nil
+      conn.port = config["port"] unless config["port"].nil?
       conn.authenticate(config["username"], config["password"])
       conn.base = config["base"]
       
